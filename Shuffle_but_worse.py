5afc48a5d482a7f04af39dfe7b5bb75198974955
André Santos <andre.pucas.santos@gmail.com>
2019-12-19 23:15:28 +0000

    v1.2 - menu with fuctioning buttons

--- Shuffle_but_worse.py
+++ Shuffle_but_worse.py
@@ -1,49 +1,65 @@
 import pygame
 import pygame.freetype
 
-def main():
+# color sets
+normal = (255, 255, 0)
+hover = (255, 255, 255)
 
-    pygame.init()
+pygame.init()
+res = (1280, 720)
+screen = pygame.display.set_mode(res)
 
-    res = (1280, 720)
-    screen = pygame.display.set_mode(res)
+image = pygame.image.load("shuffle.png")
+my_font = pygame.freetype.Font("NotoSans-Regular.ttf", 24)
 
-    image = pygame.image.load("shuffle.png")
-    my_font = pygame.freetype.Font("NotoSans-Regular.ttf", 24)
+# front page, menu with game options
+def menu():
 
     while (True):
         
         for event in pygame.event.get():
             
             if (event.type == pygame.QUIT):
+                pygame.quit()
                 exit()
         
         # background
         screen.fill((0,0,20))
         # header
         screen.blit(image, (240,20))
-        # 4x3
-        pygame.draw.rect(screen, (225, 255, 0), (570, 300, 140, 30), 2)
-        my_font.render_to(screen, (620, 305), "4x3", (255, 255, 0))
-        # 4x4
-        pygame.draw.rect(screen, (225, 255, 0), (570, 340, 140, 30), 2)
-        my_font.render_to(screen, (620, 345), "4x4", (255, 255, 0))
-        # 5x4
-        pygame.draw.rect(screen, (225, 255, 0), (570, 380, 140, 30), 2)
-        my_font.render_to(screen, (620, 385), "5x4", (255, 255, 0))
-        # 6x5
-        pygame.draw.rect(screen, (225, 255, 0), (570, 420, 140, 30), 2)
-        my_font.render_to(screen, (620, 425), "6x5", (255, 255, 0))
-        # 6x6
-        pygame.draw.rect(screen, (225, 255, 0), (570, 460, 140, 30), 2)
-        my_font.render_to(screen, (620, 465), "6x6", (255, 255, 0))
-        # exit
-        pygame.draw.rect(screen, (225, 255, 0), (570, 520, 140, 30), 2)
-        my_font.render_to(screen, (620, 525), "Exit", (255, 255, 0))
+        # buttons
+        button("4x3", 570, 300, 140, 30)
+        button("4x4", 570, 340, 140, 30)
+        button("5x4", 570, 380, 140, 30)
+        button("6x5", 570, 420, 140, 30)
+        button("6x6", 570, 460, 140, 30)
+        button("Exit", 570, 520, 140, 30, "quit")
        
-
         pygame.display.flip()
 
-main()
+# gets button message, position, area, and action
+def button(text, x, y, width, height, action = None):
+    mouse = pygame.mouse.get_pos() 
+    click = pygame.mouse.get_pressed() 
+
+
+    # if hovered
+    if x < mouse[0] < x + width and y < mouse[1] < y + height:
+        pygame.draw.rect(screen, hover, (x, y, width, height), 2)
+        my_font.render_to(screen, (x + 50, y + 5), text, hover)
+
+        # if clicked
+        if click[0] == 1 and action != None:
+            if action == "quit":
+                pygame.quit()
+                exit()
+                
+    # normal state
+    else:
+        pygame.draw.rect(screen, normal, (x, y, width, height), 2)
+        my_font.render_to(screen, (x + 50, y + 5), text, normal)
+
+# starts running here    
+menu()
 
         
