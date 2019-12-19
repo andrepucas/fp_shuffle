e2c3e79dd1b5f20ff8edd4c1611eb523c687a2d9
André Santos <andre.pucas.santos@gmail.com>
2019-12-19 12:53:33 +0000

    v1.1-menu w/text

--- Shuffle_but_worse.py
+++ Shuffle_but_worse.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.freetype
 
 def main():
 
@@ -8,6 +9,7 @@ def main():
     screen = pygame.display.set_mode(res)
 
     image = pygame.image.load("shuffle.png")
+    my_font = pygame.freetype.Font("NotoSans-Regular.ttf", 24)
 
     while (True):
         
@@ -16,16 +18,28 @@ def main():
             if (event.type == pygame.QUIT):
                 exit()
         
+        # background
         screen.fill((0,0,20))
-
+        # header
         screen.blit(image, (240,20))
-
+        # 4x3
         pygame.draw.rect(screen, (225, 255, 0), (570, 300, 140, 30), 2)
+        my_font.render_to(screen, (620, 305), "4x3", (255, 255, 0))
+        # 4x4
         pygame.draw.rect(screen, (225, 255, 0), (570, 340, 140, 30), 2)
+        my_font.render_to(screen, (620, 345), "4x4", (255, 255, 0))
+        # 5x4
         pygame.draw.rect(screen, (225, 255, 0), (570, 380, 140, 30), 2)
+        my_font.render_to(screen, (620, 385), "5x4", (255, 255, 0))
+        # 6x5
         pygame.draw.rect(screen, (225, 255, 0), (570, 420, 140, 30), 2)
+        my_font.render_to(screen, (620, 425), "6x5", (255, 255, 0))
+        # 6x6
         pygame.draw.rect(screen, (225, 255, 0), (570, 460, 140, 30), 2)
+        my_font.render_to(screen, (620, 465), "6x6", (255, 255, 0))
+        # exit
         pygame.draw.rect(screen, (225, 255, 0), (570, 520, 140, 30), 2)
+        my_font.render_to(screen, (620, 525), "Exit", (255, 255, 0))
        
 
         pygame.display.flip()
